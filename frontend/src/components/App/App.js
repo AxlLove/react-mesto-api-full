63591275e01aa095be2eb83ed855f2e25e687bf5
Aleksey Kitaev <axlstarxx@gmail.com>
2022-06-13 22:05:32 +0300

    debug

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -45,7 +45,7 @@ function App() {
     },[loggedIn]);
 
     function handleCardLike(card) {
-        const isLiked = card.likes.some(i => i.id === currentUser._id);
+        const isLiked = card.likes.some(i => i === currentUser._id);
         api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
             setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
         }).catch(console.log);
