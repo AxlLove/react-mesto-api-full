85fcfc68a636e63413fe9dfecad36e5f8a8aa825
Aleksey Kitaev <axlstarxx@gmail.com>
2022-06-13 20:51:22 +0300

    debug

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -8,7 +8,7 @@ const createCard = (req, res, next) => {
   if (!name || !link) {
     throw new ValidationError('Переданы некорректные данные при создании карточки.');
   }
-  const owner = req.user.id;
+  const owner = req.user;
   Card.create({ name, link, owner })
     .then((card) => res.send(card))
     .catch(
