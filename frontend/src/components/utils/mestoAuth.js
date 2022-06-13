40453d3a9394e7c985b34646736d5522ce4f188a
Aleksey Kitaev <axlstarxx@gmail.com>
2022-06-13 18:35:17 +0300

    debug

--- frontend/src/components/utils/mestoAuth.js
+++ frontend/src/components/utils/mestoAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'api.axlstar.mesto.nomoreparties.sbs';
+export const BASE_URL = 'https://api.axlstar.mesto.nomoreparties.sbs';
 
 const checkResponse = (response) => {
     console.log('response ok: ', response);
