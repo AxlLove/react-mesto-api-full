455fe7229647fc9f40d4a7aaed91b7cda3a8bac5
Aleksey Kitaev <axlstarxx@gmail.com>
2022-06-14 10:31:22 +0300

    Prod .env/debug

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
-const SECRET_KEY = '1609562';
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, SECRET_KEY);
+    payload = jwt.verify(token,NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     if (err.name === 'JsonWebTokenError') {
       const validationError = new UnauthorizedError('Необходима авторизация');
--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const generateToken = (payload) => {
+  console.log(NODE_ENV, JWT_SECRET);
   const token = jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+  console.log(token);
   return token;
 };
 
